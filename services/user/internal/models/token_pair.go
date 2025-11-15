ebe2b9dffe14947da119837a9c090cd70a83ce5b
deimossy <nikitakniazev01@gmail.com>
2025-11-15 15:36:33 +0500

    chore(user): add docs

--- services/user/internal/models/token_pair.go
+++ services/user/internal/models/token_pair.go
@@ -3,8 +3,8 @@ package models
 import "time"
 
 type TokenPair struct {
-	AccessToken  string
-	RefreshToken string
-	ExpiresAt    time.Time
-	UserID       string
+	AccessToken  string    `json:"access_token" example:"eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9..."`
+	RefreshToken string    `json:"refresh_token" example:"auGdrFA8HKbLrEY-2X9KTAtvzh9Qh-obHElRMtEg..."`
+	ExpiresAt    time.Time `json:"expires_at" example:"2025-11-15T10:18:37Z"`
+	UserID       string    `json:"user_id" example:"37d86688-021e-41ce-932a-841a047f0454"`
 }
